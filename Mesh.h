7ab67819cdd40266c5e19c68e08d5cdfb3172155
Charles Iroegbu <charlesiroegbu99@gmail.com>
2024-11-29 08:36:09 +0000

    Update Mesh.h
    Add mesh, plane, cube, sphere and model classes

--- Mesh.h
+++ Mesh.h
@@ -4,6 +4,8 @@
 #include "DXCore.h"
 
 
+#include "GEMLoader.h"
+
 struct Vertex
 {
 	Vec3 position;
@@ -11,6 +13,272 @@ struct Vertex
 	Vertex() {};
 };
 
+struct STATIC_VERTEX
+{
+	Vec3 pos;
+	Vec3 normal;
+	Vec3 tangent;
+	float tu;
+	float tv;
+};
+
+class Mesh {
+public:
+	ID3D11Buffer* indexBuffer;
+	ID3D11Buffer* vertexBuffer;
+	int indicesSize;
+	UINT strides;
+
+	void init(DXCOre* core ,void* vertices, int vertexSizeInBytes, int numVertices, unsigned int* indices, int numIndices) {
+		D3D11_BUFFER_DESC bd;
+		memset(&bd, 0, sizeof(D3D11_BUFFER_DESC));
+		bd.Usage = D3D11_USAGE_DEFAULT;
+		bd.ByteWidth = sizeof(unsigned int) * numIndices;
+		bd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+		D3D11_SUBRESOURCE_DATA data;
+		memset(&data, 0, sizeof(D3D11_SUBRESOURCE_DATA));
+		data.pSysMem = indices;
+		core->device->CreateBuffer(&bd, &data, &indexBuffer);
+		bd.ByteWidth = vertexSizeInBytes * numVertices;
+		bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+		data.pSysMem = vertices;
+		core->device->CreateBuffer(&bd, &data, &vertexBuffer);
+		indicesSize = numIndices;
+		strides = vertexSizeInBytes;
+	}
+
+	void init(DXCOre* core, std::vector<STATIC_VERTEX> vertices, std::vector<unsigned int> indices)
+	{
+		init(core,&vertices[0], sizeof(STATIC_VERTEX), vertices.size(), &indices[0], indices.size());
+	}
+
+	void draw(DXCOre* core) {
+		UINT offsets = 0;
+		core->devicecontext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+		core->devicecontext->IASetVertexBuffers(0, 1, &vertexBuffer, &strides, &offsets);
+		core->devicecontext->IASetIndexBuffer(indexBuffer, DXGI_FORMAT_R32_UINT, 0);
+		core->devicecontext->DrawIndexed(indicesSize, 0, 0);
+	}
+};
+
+class Plane {
+public:
+	Mesh geometry;
+
+	STATIC_VERTEX addVertex(Vec3 p, Vec3 n, float tu, float tv)
+	{
+		STATIC_VERTEX v;
+		v.pos = p;
+		v.normal = n;
+		//Frame frame;
+		//frame.fromVector(n);c
+		//v.tangent = frame.u; // For now
+		v.tangent = Vec3(0,0,0); // For now
+		v.tu = tu;
+		v.tv = tv;
+		return v;
+	}
+
+	void init(DXCOre* core) {
+		std::vector<STATIC_VERTEX> vertices;
+		vertices.push_back(addVertex(Vec3(-15, 0, -15), Vec3(0, 1, 0), 0, 0));
+		vertices.push_back(addVertex(Vec3(15, 0, -15), Vec3(0, 1, 0), 1, 0));
+		vertices.push_back(addVertex(Vec3(-15, 0, 15), Vec3(0, 1, 0), 0, 1));
+		vertices.push_back(addVertex(Vec3(15, 0, 15), Vec3(0, 1, 0), 1, 1));
+		std::vector<unsigned int> indices;
+		indices.push_back(2); indices.push_back(1); indices.push_back(0);
+		indices.push_back(1); indices.push_back(2); indices.push_back(3);
+		geometry.init(core, vertices, indices);
+
+	}
+};
+
+class Cube {
+public:
+	Mesh geometry;
+
+	STATIC_VERTEX addVertex(Vec3 p, Vec3 n, float tu, float tv)
+	{
+		STATIC_VERTEX v;
+		v.pos = p;
+		v.normal = n;
+		//Frame frame;
+		//frame.fromVector(n);c
+		//v.tangent = frame.u; // For now
+		v.tangent = Vec3(0,0,0); // For now
+		v.tu = tu;
+		v.tv = tv;
+		return v;
+	}
+
+	void init(DXCOre* core) {
+		std::vector<STATIC_VERTEX> vertices;
+		Vec3 p0 = Vec3(-1.0f, -1.0f, -1.0f);
+		Vec3 p1 = Vec3(1.0f, -1.0f, -1.0f);
+		Vec3 p2 = Vec3(1.0f, 1.0f, -1.0f);
+		Vec3 p3 = Vec3(-1.0f, 1.0f, -1.0f);
+		Vec3 p4 = Vec3(-1.0f, -1.0f, 1.0f);
+		Vec3 p5 = Vec3(1.0f, -1.0f, 1.0f);
+		Vec3 p6 = Vec3(1.0f, 1.0f, 1.0f);
+		Vec3 p7 = Vec3(-1.0f, 1.0f, 1.0f);
+
+		vertices.push_back(addVertex(p0, Vec3(0.0f, 0.0f, -1.0f), 0.0f, 1.0f));
+		vertices.push_back(addVertex(p1, Vec3(0.0f, 0.0f, -1.0f), 1.0f, 1.0f));
+		vertices.push_back(addVertex(p2, Vec3(0.0f, 0.0f, -1.0f), 1.0f, 0.0f));
+		vertices.push_back(addVertex(p3, Vec3(0.0f, 0.0f, -1.0f), 0.0f, 0.0f));
+
+		vertices.push_back(addVertex(p5, Vec3(0.0f, 0.0f, 1.0f), 0.0f, 1.0f));
+		vertices.push_back(addVertex(p4, Vec3(0.0f, 0.0f, 1.0f), 1.0f, 1.0f));
+		vertices.push_back(addVertex(p7, Vec3(0.0f, 0.0f, 1.0f), 1.0f, 0.0f));
+		vertices.push_back(addVertex(p6, Vec3(0.0f, 0.0f, 1.0f), 0.0f, 0.0f));
+
+		vertices.push_back(addVertex(p4, Vec3(-1.0f, 0.0f, 0.0f), 0.0f, 1.0f));
+		vertices.push_back(addVertex(p0, Vec3(-1.0f, 0.0f, 0.0f), 1.0f, 1.0f));
+		vertices.push_back(addVertex(p3, Vec3(-1.0f, 0.0f, 0.0f), 1.0f, 0.0f));
+		vertices.push_back(addVertex(p7, Vec3(-1.0f, 0.0f, 0.0f), 0.0f, 0.0f));
+
+
+		vertices.push_back(addVertex(p1, Vec3(1.0f, 0.0f, 0.0f), 0.0f, 1.0f));
+		vertices.push_back(addVertex(p5, Vec3(1.0f, 0.0f, 0.0f), 1.0f, 1.0f));
+		vertices.push_back(addVertex(p6, Vec3(1.0f, 0.0f, 0.0f), 1.0f, 0.0f));
+		vertices.push_back(addVertex(p2, Vec3(1.0f, 0.0f, 0.0f), 0.0f, 0.0f));
+
+		vertices.push_back(addVertex(p3, Vec3(0.0f, 1.0f, 0.0f), 0.0f, 1.0f));
+		vertices.push_back(addVertex(p2, Vec3(0.0f, 1.0f, 0.0f), 1.0f, 1.0f));
+		vertices.push_back(addVertex(p6, Vec3(0.0f, 1.0f, 0.0f), 1.0f, 0.0f));
+		vertices.push_back(addVertex(p7, Vec3(0.0f, 1.0f, 0.0f), 0.0f, 0.0f));
+
+		vertices.push_back(addVertex(p4, Vec3(0.0f, -1.0f, 0.0f), 0.0f, 1.0f));
+		vertices.push_back(addVertex(p5, Vec3(0.0f, -1.0f, 0.0f), 1.0f, 1.0f));
+		vertices.push_back(addVertex(p1, Vec3(0.0f, -1.0f, 0.0f), 1.0f, 0.0f));
+		vertices.push_back(addVertex(p0, Vec3(0.0f, -1.0f, 0.0f), 0.0f, 0.0f));
+
+
+		std::vector<unsigned int> indices;
+		indices.push_back(0); indices.push_back(1); indices.push_back(2);
+		indices.push_back(0); indices.push_back(2); indices.push_back(3);
+		indices.push_back(4); indices.push_back(5); indices.push_back(6);
+		indices.push_back(4); indices.push_back(6); indices.push_back(7);
+		indices.push_back(8); indices.push_back(9); indices.push_back(10);
+		indices.push_back(8); indices.push_back(10); indices.push_back(11);
+		indices.push_back(12); indices.push_back(13); indices.push_back(14);
+		indices.push_back(12); indices.push_back(14); indices.push_back(15);
+		indices.push_back(16); indices.push_back(17); indices.push_back(18);
+		indices.push_back(16); indices.push_back(18); indices.push_back(19);
+		indices.push_back(20); indices.push_back(21); indices.push_back(22);
+		indices.push_back(20); indices.push_back(22); indices.push_back(23);
+
+		geometry.init(core, vertices, indices);
+
+	}
+};
+
+class Sphere {
+public:
+	Mesh geometry;
+	int rings;
+	int segments; 
+	float radius;
+
+	Sphere(int _rings, int _segments, float _radius) {
+		rings = _rings;
+		segments = _segments;
+		radius = _radius;
+	}
+
+	STATIC_VERTEX addVertex(Vec3 p, Vec3 n, float tu, float tv)
+	{
+		STATIC_VERTEX v;
+		v.pos = p;
+		v.normal = n;
+		//Frame frame;
+		//frame.fromVector(n);c
+		//v.tangent = frame.u; // For now
+		v.tangent = Vec3(0, 0, 0); // For now
+		v.tu = tu;
+		v.tv = tv;
+		return v;
+	}
+
+	void init(DXCOre* core) {
+		std::vector<STATIC_VERTEX> vertices;
+		for (int lat = 0; lat <= rings; lat++) {
+			float theta = lat * M_PI / rings;
+			float sinTheta = sinf(theta);
+			float cosTheta = cosf(theta);
+			for (int lon = 0; lon <= segments; lon++) {
+				float phi = lon * 2.0f * M_PI / segments;
+				float sinPhi = sinf(phi);
+				float cosPhi = cosf(phi);
+				Vec3 position(radius * sinTheta * cosPhi, radius * cosTheta, radius * sinTheta * sinPhi);
+				Vec3 normal = position.normalize();
+				float tu = 1.0f - (float)lon / segments;
+				float tv = 1.0f - (float)lat / rings;
+				vertices.push_back(addVertex(position, normal, tu, tv));
+			}
+		}
+
+		std::vector<unsigned int> indices;
+		for (int lat = 0; lat < rings; lat++)
+		{
+			for (int lon = 0; lon < segments; lon++)
+			{
+				int current = lat * (segments + 1) + lon;
+				int next = current + segments + 1;
+				indices.push_back(current);
+				indices.push_back(next);
+				indices.push_back(current + 1);
+				indices.push_back(current + 1);
+				indices.push_back(next);
+				indices.push_back(next + 1);
+			}
+		}
+		geometry.init(core, vertices, indices);
+
+	}
+};
+
+class Model{
+	std::vector<Mesh> meshes;
+public:
+	STATIC_VERTEX addVertex(Vec3 p, Vec3 n, float tu, float tv)
+	{
+		STATIC_VERTEX v;
+		v.pos = p;
+		v.normal = n;
+		//Frame frame;
+		//frame.fromVector(n);c
+		//v.tangent = frame.u; // For now
+		v.tangent = Vec3(0, 0, 0); // For now
+		v.tu = tu;
+		v.tv = tv;
+		return v;
+	}
+
+	void init(DXCOre* core , std::string filename) {
+		GEMLoader::GEMModelLoader loader;
+		std::vector<GEMLoader::GEMMesh> gemmeshes;
+		loader.load(filename, gemmeshes);
+		for (int i = 0; i < gemmeshes.size(); i++) {
+			Mesh mesh;
+			std::vector<STATIC_VERTEX> vertices;
+			for (int j = 0; j < gemmeshes[i].verticesStatic.size(); j++) {
+				STATIC_VERTEX v;
+				memcpy(&v, &gemmeshes[i].verticesStatic[j], sizeof(STATIC_VERTEX));
+				vertices.push_back(v);
+			}
+			mesh.init(core, vertices, gemmeshes[i].indices);
+			meshes.push_back(mesh);
+		}
+	}
+
+	void draw(DXCOre* core) {
+		for (int i = 0; i < meshes.size(); i++)
+		{
+			meshes[i].draw(core);
+		}
+	}
+};
 
 class Triangle {
 public:
