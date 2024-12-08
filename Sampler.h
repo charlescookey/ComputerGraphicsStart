c72c0a74f5bea93417c2ad8c8735fb84af570525
Charles Iroegbu <charlesiroegbu99@gmail.com>
2024-12-08 08:56:28 +0000

    sampler

---
+++ Sampler.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "DXCore.h"
+
+class Sampler {
+public:
+	ID3D11SamplerState* state;
+
+	void init(DXCOre* core) {
+		D3D11_SAMPLER_DESC samplerDesc;
+		ZeroMemory(&samplerDesc, sizeof(samplerDesc));
+		samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+		samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+		samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+		samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+		samplerDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+		samplerDesc.MinLOD = 0;
+		samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
+		core->device->CreateSamplerState(&samplerDesc, &state);
+
+	}
+};
