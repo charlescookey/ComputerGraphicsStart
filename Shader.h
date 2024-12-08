58116ba1b357016e1ecbb62f696b89333c7fb87e
Charles Iroegbu <charlesiroegbu99@gmail.com>
2024-12-08 08:55:35 +0000

    updating shader code for animated meshes

--- Shader.h
+++ Shader.h
@@ -28,6 +28,13 @@ struct alignas(16) ConstantBuffer2
 
 ID3D11Buffer* constantBuffer;
 
+std::string readFile(std::string filename) {
+	std::ifstream infile(filename);
+	std::stringstream output;
+	output << infile.rdbuf();
+	infile.close();
+	return output.str();
+}
 
 class Shader {
 public:
@@ -36,32 +43,19 @@ public:
 
 	ID3D11InputLayout* layout;
 
-
-	std::string readFile(std::string filename) {
-		std::ifstream infile(filename);
-		std::stringstream output;
-		output << infile.rdbuf();
-		infile.close();
-		return output.str();
-	}
-
 	void init(std::string VS_filename , std::string PS_filename, DXCOre* core) {
 		std::string vs = readFile(VS_filename);
 		std::string ps = readFile(PS_filename);
 
-
-		//here?
-
-		//D3D11_BUFFER_DESC bd;
-		//bd.Usage = D3D11_USAGE_DYNAMIC;
-		//bd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
-		//bd.MiscFlags = 0;
-		//D3D11_SUBRESOURCE_DATA data;
-		//bd.ByteWidth = sizeof(ConstantBuffer2);
-		//bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
-		//core->device->CreateBuffer(&bd, NULL, &constantBuffer);
-
 		loadVS(core, vs);
+		loadPS(core, ps);
+	}
+
+	void init_anim(std::string VS_filename, std::string PS_filename, DXCOre* core) {
+		std::string vs = readFile(VS_filename);
+		std::string ps = readFile(PS_filename);
+
+		loadVS_Skeletal(core, vs);
 		loadPS(core, ps);
 	}
 
@@ -70,29 +64,6 @@ public:
 	std::map<std::string, int> textureBindPointsVS;
 	std::map<std::string, int> textureBindPointsPS;
 
-	void compileVS(std::string vertexShaderHLSL, DXCOre* core) {
-		ID3DBlob* compiledVertexShader;
-		ID3DBlob* status;
-		HRESULT hr = D3DCompile(vertexShaderHLSL.c_str(), strlen(vertexShaderHLSL.c_str()), NULL, NULL, NULL, "VS", "vs_5_0", 0, 0, &compiledVertexShader, &status);
-		if (FAILED(hr))
-		{
-			MessageBoxA(NULL, (char*)status->GetBufferPointer(), "Vertex Shader Error", 0);
-			exit(0);
-		}
-		core->device->CreateVertexShader(compiledVertexShader->GetBufferPointer(), compiledVertexShader->GetBufferSize(), NULL, &vertexShader);
-
-
-		D3D11_INPUT_ELEMENT_DESC layoutDesc[] =
-		{
-			{ "POS", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, 							D3D11_INPUT_PER_VERTEX_DATA, 0 },
-			{ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, 							D3D11_INPUT_PER_VERTEX_DATA, 0 },
-			{ "TANGENT", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, 							D3D11_INPUT_PER_VERTEX_DATA, 0 },
-			{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, 							D3D11_INPUT_PER_VERTEX_DATA, 0 },
-		};
-
-		core->device->CreateInputLayout(layoutDesc, 4, compiledVertexShader->GetBufferPointer(), compiledVertexShader->GetBufferSize(), &layout);
-
-	}
 
 	void loadVS(DXCOre* core, std::string vertexShaderHLSL)
 	{
@@ -120,18 +91,31 @@ public:
 		reflection.build(core, compiledVertexShader, vsConstantBuffers, textureBindPointsVS, ShaderStage::VertexShader);
 	}
 
-	
-
-	void compilePS(std::string pixelShaderHLSL, DXCOre* core) {
-		ID3DBlob* compiledPixelShader;
+	void loadVS_Skeletal(DXCOre* core, std::string vertexShaderHLSL)
+	{
+		ID3DBlob* compiledVertexShader;
 		ID3DBlob* status;
-		HRESULT hr = D3DCompile(pixelShaderHLSL.c_str(), strlen(pixelShaderHLSL.c_str()), NULL, NULL, NULL, "PS", "ps_5_0", 0, 0, &compiledPixelShader, &status);
+		HRESULT hr = D3DCompile(vertexShaderHLSL.c_str(), strlen(vertexShaderHLSL.c_str()), NULL, NULL, NULL, "VS", "vs_5_0", 0, 0, &compiledVertexShader, &status);
 		if (FAILED(hr))
 		{
-			MessageBoxA(NULL, (char*)status->GetBufferPointer(), "Pixel Shader Error", 0);
+			MessageBoxA(NULL, (char*)status->GetBufferPointer(), "Vertex Shader Error", 0);
 			exit(0);
 		}
-		core->device->CreatePixelShader(compiledPixelShader->GetBufferPointer(), compiledPixelShader->GetBufferSize(), NULL, &pixelShader);
+
+		core->device->CreateVertexShader(compiledVertexShader->GetBufferPointer(), compiledVertexShader->GetBufferSize(), NULL, &vertexShader);
+
+		D3D11_INPUT_ELEMENT_DESC layoutDesc[] = {
+		{ "POS", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, 								D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, 								D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "TANGENT", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, 								D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, 								D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "BONEIDS", 0, DXGI_FORMAT_R32G32B32A32_UINT, 0, D3D11_APPEND_ALIGNED_ELEMENT, 							D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "BONEWEIGHTS", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, 							D3D11_INPUT_PER_VERTEX_DATA, 0 },
+			};
+		core->device->CreateInputLayout(layoutDesc, 6, compiledVertexShader->GetBufferPointer(), compiledVertexShader->GetBufferSize(), &layout);
+
+		ConstantBufferReflection reflection;
+		reflection.build(core, compiledVertexShader, vsConstantBuffers, textureBindPointsVS, ShaderStage::VertexShader);
 	}
 
 	void loadPS(DXCOre* core, std::string pixelShaderHLSL)
@@ -169,7 +153,13 @@ public:
 	{
 		updateConstant(constantBufferName, variableName, data, psConstantBuffers);
 	}
-
+	void updateTexturePS(DXCOre* core, std::string name, ID3D11ShaderResourceView* tex){
+		//core->devicecontext->PSSetShaderResources(0, 1, &srv);
+		core->devicecontext->PSSetShaderResources(textureBindPointsPS[name], 1, &tex);
+	}
+	void updateSampler(DXCOre* core, ID3D11SamplerState* state) {
+		core->devicecontext->PSSetSamplers(0, 1, &state);
+	}
 	void apply(DXCOre* core) {
 
 		////here?!
@@ -196,3 +186,5 @@ public:
 	}
 
 };
+
+//shaders->updateConstantVS("Animated", "staticMeshBuffer", "W", &w);
