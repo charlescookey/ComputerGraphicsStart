653a36ef9e4ade0d2414b4946a23a59aa29597a8
Charles Iroegbu <charlesiroegbu99@gmail.com>
2024-12-13 22:39:32 +0000

    added new function for instancing

--- Shader.h
+++ Shader.h
@@ -59,6 +59,14 @@ public:
 		loadPS(core, ps);
 	}
 
+	void init_inst(std::string VS_filename, std::string PS_filename, DXCOre* core) {
+		std::string vs = readFile(VS_filename);
+		std::string ps = readFile(PS_filename);
+
+		loadVS_Instance(core, vs);
+		loadPS(core, ps);
+	}
+
 	std::vector<ConstantBuffer> psConstantBuffers;
 	std::vector<ConstantBuffer> vsConstantBuffers;
 	std::map<std::string, int> textureBindPointsVS;
@@ -87,6 +95,34 @@ public:
 		};
 
 		core->device->CreateInputLayout(layoutDesc, 4, compiledVertexShader->GetBufferPointer(), compiledVertexShader->GetBufferSize(), &layout);
+		ConstantBufferReflection reflection;
+		reflection.build(core, compiledVertexShader, vsConstantBuffers, textureBindPointsVS, ShaderStage::VertexShader);
+	}
+
+	void loadVS_Instance(DXCOre* core, std::string vertexShaderHLSL)
+	{
+		ID3DBlob* compiledVertexShader;
+		ID3DBlob* status;
+		HRESULT hr = D3DCompile(vertexShaderHLSL.c_str(), strlen(vertexShaderHLSL.c_str()), NULL, NULL, NULL, "VS", "vs_5_0", 0, 0, &compiledVertexShader, &status);
+		if (FAILED(hr))
+		{
+			MessageBoxA(NULL, (char*)status->GetBufferPointer(), "Vertex Shader Error", 0);
+			exit(0);
+		}
+
+		core->device->CreateVertexShader(compiledVertexShader->GetBufferPointer(), compiledVertexShader->GetBufferSize(), NULL, &vertexShader);
+
+		D3D11_INPUT_ELEMENT_DESC layoutDesc[] =
+		{
+			{ "POS", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, 							D3D11_INPUT_PER_VERTEX_DATA, 0 },
+			{ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, 							D3D11_INPUT_PER_VERTEX_DATA, 0 },
+			{ "TANGENT", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, 							D3D11_INPUT_PER_VERTEX_DATA, 0 },
+			{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, 							D3D11_INPUT_PER_VERTEX_DATA, 0 },
+			{ "INSTANCEPOSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 1, 0, 							D3D11_INPUT_PER_INSTANCE_DATA, 1 },
+		};
+
+
+		core->device->CreateInputLayout(layoutDesc, 5, compiledVertexShader->GetBufferPointer(), compiledVertexShader->GetBufferSize(), &layout);
 		ConstantBufferReflection reflection;
 		reflection.build(core, compiledVertexShader, vsConstantBuffers, textureBindPointsVS, ShaderStage::VertexShader);
 	}
@@ -157,8 +193,8 @@ public:
 		//core->devicecontext->PSSetShaderResources(0, 1, &srv);
 		core->devicecontext->PSSetShaderResources(textureBindPointsPS[name], 1, &tex);
 	}
-	void updateSampler(DXCOre* core, ID3D11SamplerState* state) {
-		core->devicecontext->PSSetSamplers(0, 1, &state);
+	void updateSampler(DXCOre* core, ID3D11SamplerState* state, int num) {
+		core->devicecontext->PSSetSamplers(num, 1, &state);
 	}
 	void apply(DXCOre* core) {
 
--- ShaderManager.h
+++ ShaderManager.h
@@ -5,7 +5,8 @@
 enum ShaderType
 {
 	StaticShader,
-	AnimatedShader
+	AnimatedShader,
+	InstanceShader
 };
 
 class ShaderManager {
@@ -14,8 +15,10 @@ public:
 	void init(ShaderType type , std::string shadername ,std::string VS_filename, std::string PS_filename, DXCOre* core) {
 		if (type == ShaderType::StaticShader)
 			shaders[shadername].init(VS_filename, PS_filename, core);
-		else
+		else if (type == ShaderType::AnimatedShader)
 			shaders[shadername].init_anim(VS_filename, PS_filename, core);
+		else if (type == ShaderType::InstanceShader)
+			shaders[shadername].init_inst(VS_filename, PS_filename, core);
 	}
 
 	void updateConstantVS(std::string shadername, std::string constantBufferName, std::string variableName, void* data)
@@ -29,8 +32,8 @@ public:
 	void updateTexturePS(std::string shadername,DXCOre* core, std::string name, ID3D11ShaderResourceView* tex) {
 		shaders[shadername].updateTexturePS(core, name, tex);
 	}
-	void updateSampler(std::string shadername, DXCOre* core, ID3D11SamplerState* state) {
-		shaders[shadername].updateSampler(core, state);
+	void updateSampler(std::string shadername, DXCOre* core, ID3D11SamplerState* state, int num) {
+		shaders[shadername].updateSampler(core, state, num);
 	}
 	void apply(std::string shadername, DXCOre* core) {
 		shaders[shadername].apply(core);
