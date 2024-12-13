d156f1ab4cef836a0d555f3e1eb8c6d24875ffaa
Charles Iroegbu <charlesiroegbu99@gmail.com>
2024-12-13 22:39:54 +0000

    added new sampler types

--- Sampler.h
+++ Sampler.h
@@ -16,6 +16,72 @@ public:
 		samplerDesc.MinLOD = 0;
 		samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
 		core->device->CreateSamplerState(&samplerDesc, &state);
+	}
 
+	void init_clamp(DXCOre* core) {
+		D3D11_SAMPLER_DESC samplerDesc;
+		ZeroMemory(&samplerDesc, sizeof(samplerDesc));
+		samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+		samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
+		samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
+		samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
+		samplerDesc.MipLODBias = 0.0f;
+		samplerDesc.MaxAnisotropy = 1;
+		samplerDesc.ComparisonFunc = D3D11_COMPARISON_ALWAYS;
+		samplerDesc.BorderColor[0] = 0;
+		samplerDesc.BorderColor[1] = 0;
+		samplerDesc.BorderColor[2] = 0;
+		samplerDesc.BorderColor[3] = 0;
+		samplerDesc.MinLOD = 0;
+		samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
+		core->device->CreateSamplerState(&samplerDesc, &state);
+	}
+
+	void init_wrap(DXCOre* core) {
+		D3D11_SAMPLER_DESC samplerDesc;
+		ZeroMemory(&samplerDesc, sizeof(samplerDesc));
+		samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+		samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+		samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+		samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+		samplerDesc.MipLODBias = 0.0f;
+		samplerDesc.MaxAnisotropy = 1;
+		samplerDesc.ComparisonFunc = D3D11_COMPARISON_ALWAYS;
+		samplerDesc.BorderColor[0] = 0;
+		samplerDesc.BorderColor[1] = 0;
+		samplerDesc.BorderColor[2] = 0;
+		samplerDesc.BorderColor[3] = 0;
+		samplerDesc.MinLOD = 0;
+		samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
+		core->device->CreateSamplerState(&samplerDesc, &state);
+	}
+
+
+
+};
+
+
+enum SamplerType
+{
+	Wrap,
+	Clamp,
+	Default
+};
+
+class SamplerManager {
+	std::map<std::string, Sampler> samplers;
+public:
+	void init(SamplerType type, std::string samplername, DXCOre* core) {
+		if (type == SamplerType::Wrap)
+			samplers[samplername].init_wrap(core);
+		else if (type == SamplerType::Wrap)
+			samplers[samplername].init_clamp(core);
+		else
+			samplers[samplername].init(core);
+	}
+
+	ID3D11SamplerState* find(std::string name)
+	{
+		return samplers[name].state;
 	}
 };
