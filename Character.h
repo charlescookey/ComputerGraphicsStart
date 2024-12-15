3cabde936148adf68d0b5795102ca636cbceba64
Charles Iroegbu <charlesiroegbu99@gmail.com>
2024-12-15 08:56:57 +0000

    Added a new character class

---
+++ Character.h
@@ -0,0 +1,98 @@
+#pragma once
+#include "SkeletalMesh.h"
+
+
+enum AnimState
+{
+	Idle,
+	Run,
+	Attack,
+	Fire,
+	React,
+	Death
+};
+
+std::string AnimStateToString(AnimState state) {
+	switch (state) {
+	case AnimState::Idle: return "Idle";
+	case AnimState::Run: return "Run";
+	case AnimState::Attack: return "attack";
+	case AnimState::Fire: return "firing rifle";
+	case AnimState::React: return "roar";
+	case AnimState::Death: return "death";
+	default: return "Idle";
+	}
+}
+
+class Character {
+private:
+	AnimationInstance animInstance;
+	AnimState state;
+
+	int health ;
+	float reactTime = 0.f;
+	bool react = false;
+	
+	float deathTime = 0.f;
+	bool dead = false;
+	bool show = true;
+public:
+	AnimatedModel animModel;
+
+	void init(DXCOre* core, std::string filename) {
+		animModel.init(core, filename);
+		animInstance.animation = &animModel.animation;
+		health = 100;
+		state = AnimState::Idle;
+	}
+
+	void update(float dt) {
+		if (dead) {
+			deathTime += dt;
+		}
+
+		if (deathTime > 4.f) {
+			deathTime = 0.f;
+			show = false;
+			state = AnimState::Idle;
+		}
+
+		if (react) {
+			reactTime += dt;
+		}
+
+		if (reactTime > 0.5f) {
+			reactTime = 0.f;
+			react = false;
+			state = AnimState::Idle;
+		}
+		
+	}
+
+	void takeDamage(int damage) {
+		if (dead)return;
+		state = AnimState::Attack;
+		health -= damage;
+		health = max(0, health);
+		react = true;
+		
+		if (health == 0) {
+			dead = true;
+			react = false;
+			state = AnimState::Death;
+		}
+
+		std::cout << health << "\n";
+	}
+
+	void draw(float dt, ShaderManager& shaders, std::string shadername, DXCOre* core, TextureManager* textures) {
+		if (!show)return;
+		update(dt);
+		animInstance.update(AnimStateToString(state), dt);
+		//animInstance.update("Idle", dt);
+		shaders.updateConstantVS("AnimatedModel", "animatedMeshBuffer", "bones", animInstance.matrices);
+		animModel.draw(shaders, "AnimatedModel", core, textures);
+	}
+
+
+};
